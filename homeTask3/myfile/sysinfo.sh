43ce7037da71d2d0e4434b515a0fc505db01cb7a
ArtMelent <artem.melent@gmail.com>
2022-12-07 22:34:11 +0200

    hw6-7

--- homeTask3/myfile/sysinfo.sh
+++ homeTask3/myfile/sysinfo.sh
@@ -6,10 +6,10 @@ free -m >> /var/log/sysinfo.log
 
 df -h >> /var/log/sysinfo.log
 
-ss -tulpn >> /var/log/sysinfo.log
+ss -tulpn >> /var/log/sysinfo.log #для чого ми це використовуэм
 
 ping -c2 ukr.net >> /var/log/sysinfo.log
 
-find / -perm -4000 >> /var/log/sysinfo.log
+find / -perm -4000 >> /var/log/sysinfo.log #suit id це
 
 echo "----------------------------------------" >> /var/log/sysinfo.log
